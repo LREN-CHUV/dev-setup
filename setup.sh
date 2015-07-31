558e765524c3dc21d5382568867c2a22bc1f2801
Ludovic Claude <ludovic.claude54@gmail.com>
2015-07-31 17:12:20 +0200

    Install Docker-compose and Maven

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-ansible-playbook --ask-become-pass -i envs/local/etc/ansible/ \
+[ -x /usr/bin/ansible ] || (
+
+  # Install Ansible
+  sudo apt-get install -y python-setuptools python-yaml python-jinja2 python-paramiko python-keyczar
+  sudo easy_install pip
+  sudo pip install ansible
+
+)
+
+ansible-playbook --ask-become-pass -i envs/local/etc/ansible/ $@ \
   setup.yml
