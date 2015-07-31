da68508ecf5c86cf93fd17db29b5537887aa3eae
Ludovic Claude <ludovic.claude54@gmail.com>
2015-07-31 17:27:58 +0200

    Track script execution directory

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     $( cd -P "$( dirname "$SOURCE" )" )
+     pwd
+}
+
+cd $(get_script_dir)
+
 [ -x /usr/bin/ansible ] || (
 
   # Install Ansible
