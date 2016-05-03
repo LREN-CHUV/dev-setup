42333eeb0c9763198fa1eac6fbd612124a36b729
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-05-03 11:53:53 +0200

    Revert to Java 6u60, works on Mirco's desktop

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ cd $(get_script_dir)
 
 [ -d roles/docker/tasks ] || ./after-git-clone.sh
 
-ANSIBLE_OPTS="-e user=$USER"
+ANSIBLE_OPTS="-e user=$USER -vvvv"
 
 PS3='Select your role: '
 options=("Backend developer" "Frontend developer" "Algorithms developer" "System administrator" "All")
